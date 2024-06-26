6915d184a8e048f63da20020939b4505f5f7fe05
sanaa ennaji <sanaaennaji93@gmail.com>
2024-06-26 15:32:51 +0100

    add

---
+++ BS/resources/views/home.blade.php
@@ -0,0 +1,23 @@
+@include('components/header')
+
+<body>
+   
+@include("components/navs")
+
+    @include("components.statistic")
+@include("components/founder")
+@include("components/contact")
+    @include("components/fouter")
+    <script src="https://unpkg.com/flowbite@1.4.7/dist/flowbite.js"></script>
+</body>
+
+</html>
+
+
+
+
+
+
+
+
+                       
