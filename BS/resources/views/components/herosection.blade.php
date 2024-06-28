c2f9a307332b49665e2658e84323322952d38f1b
sanaa ennaji <sanaaennaji93@gmail.com>
2024-06-28 17:08:59 +0100

    ADD

--- BS/resources/views/components/herosection.blade.php
+++ BS/resources/views/components/herosection.blade.php
@@ -1,7 +1,7 @@
-<section class=" dark:bg-gray-900 mx-8">
-    <div class="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
-        <div class="mr-auto place-self-center lg:col-span-7">
-            <h1 class="max-w-2xl mb-4 text-4xl font-extrabold leading-none md:text-5xl xl:text-6xl dark:text-white">Payments tool for software companies</h1>
+<section class=" dark:bg-gray-900 mx-8" style="hight:60vh;">
+    <div class="grid max-w-screen-xl  px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
+        <div class="mr-auto place-self-center lg:col-span-7 mb-60">
+            <h1 class="max-w-2xl mb-5 text-4xl font-extrabold leading-none md:text-5xl xl:text-6xl dark:text-white">Payments tool for software companies</h1>
             <p class="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">From checkout to global sales tax compliance, companies around the world use Flowbite to simplify their payment stack.</p>
             <a href="#" class="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
                 Get started
@@ -11,11 +11,8 @@
                contact 
             </a> 
         </div>
-        {{-- <div class="hidden lg:mt-0 lg:col-span-5 lg:flex">
-            <img src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/hero/phone-mockup.png" alt="mockup">
-        </div>                 --}}
-        <div id="id" class="hidden lg:mt-2 lg:col-span-5 lg:flex h-[90%] w-90  bg-blue-500" style="border-radius: 26% 26% 26% 26% / 36% 38% 28% 0%;">
-            <img src="AP/bggo.png" alt="mockup">
+        <div id="id" class="hidden lg:mt-2 lg:col-span-5 lg:flex   bg-blue-500" style="border-radius: 26% 26% 26% 26% / 36% 38% 28% 0%; height: 60%; width:80;">
+            <img src="AP/D.png" alt="mockup" class=" h-4/4 mx-auto">
         </div>
         <style>
             
