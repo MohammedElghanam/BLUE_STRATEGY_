7d36cb05b2c76ce9534c204210e319d84ecca380
Mohammed Elghanam <144786058+MohammedElghanam@users.noreply.github.com>
2024-07-01 10:07:23 +0100

    update

---
+++ BS/resources/views/booking.blade.php
@@ -0,0 +1,8 @@
+@include('components/header')
+
+<body>
+    @include("components/nav2")
+    @include("components/booking")
+    @include("components/footer")
+</body>
+</html>
--- BS/resources/views/home.blade.php
+++ BS/resources/views/home.blade.php
@@ -7,8 +7,8 @@
  @include("components.statistics")
 @include("components/founder")
 @include("components/contacts")
-    @include("components/footer")
-    <script src="https://unpkg.com/flowbite@1.4.7/dist/flowbite.js"></script>
+@include("components/footer")
+<script src="https://unpkg.com/flowbite@1.4.7/dist/flowbite.js"></script>
 </body>
 
 </html>
