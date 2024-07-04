1bb1b6cce4df925115df8b490e4ccf2edcdddc52
Mohammed Elghanam <144786058+MohammedElghanam@users.noreply.github.com>
2024-07-04 19:54:03 +0100

    Update herosection.blade.php

--- main/resources/views/components/herosection.blade.php
+++ main/resources/views/components/herosection.blade.php
@@ -1,8 +1,41 @@
 <section class=" mx-8 pt-16" style="height: 80vh;">
     <div class="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12 h-full">
-        <div class="mr-auto place-self-center lg:col-span-7 mb-60">
+        <div class="mr-auto place-self-center lg:col-span-7 mb-60 ">
             <h1 class="max-w-2xl mb-5 text-4xl font-extrabold leading-none md:text-5xl xl:text-6xl dark:text-white">Payments tool for software companies</h1>
-            <p class="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">From checkout to global sales tax compliance, companies around the world use Flowbite to simplify their payment stack.</p>
+            <p class="max-w-2xl font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">From checkout to global sales tax compliance, companies around the world use Flowbite to simplify their payment stack.</p>
+            <div class=" grid grid-cols-6 gap-4 w-3/4 mb-10">
+                <div class=" p-1 border-2 border-blue-800 rounded-md">
+                    <div class=" flex justify-center items-center p-2 rounded-full  bg-blue-800 col-span-1 blur-2xl">
+                        <img class=" w-12 h-12" src="AP/20.png" alt="">
+                    </div>
+                </div>
+                <div class=" p-1 border-2 border-blue-800 rounded-md">
+                    <div class=" flex justify-center items-center p-2 rounded-full  bg-blue-800 col-span-1 blur-2xl">
+                        <img class=" w-12 h-12" src="AP/21.png" alt="">
+                    </div>
+                </div>
+                <div class=" p-1 border-2 border-blue-800 rounded-md">
+                    <div class=" flex justify-center items-center p-2 rounded-full  bg-blue-800 col-span-1 blur-2xl">
+                        <img class=" w-12 h-12" src="AP/23.png" alt="">
+                    </div>
+                </div>
+                <div class=" p-1 border-2 border-blue-800 rounded-md">
+                    <div class=" flex justify-center items-center p-2 rounded-full  bg-blue-800 col-span-1 blur-2xl">
+                        <img class=" w-12 h-12" src="AP/24.png" alt="">
+                    </div>
+                </div>
+                <div class=" p-1 border-2 border-blue-800 rounded-md">
+                    <div class=" flex justify-center items-center p-2 rounded-full  bg-blue-800 col-span-1 blur-2xl">
+                        <img class=" w-12 h-12" src="AP/25.png" alt="">
+                    </div>
+                </div>
+                <div class=" p-1 border-2 border-blue-800 rounded-md">
+                    <div class=" flex justify-center items-center p-2 rounded-full  bg-blue-800 col-span-1 blur-2xl">
+                        <img class=" w-12 h-12" src="AP/26.png" alt="">
+                    </div>
+                </div>
+                
+            </div>
             <a href="{{route('booking_now')}}" class="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
                 Booking Now 
                 <svg class="w-5 h-5 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
