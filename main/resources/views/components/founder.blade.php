d6a2269127949f691f8176772d9780494df80332
Mohammed Elghanam <144786058+MohammedElghanam@users.noreply.github.com>
2024-07-04 17:30:03 +0100

    update

--- main/resources/views/components/founder.blade.php
+++ main/resources/views/components/founder.blade.php
@@ -1,23 +1,95 @@
 <style>
 .BG{
-    background-color: #0066a5;
+    background-color: #9FD3C7;
 }
 </style>
 
 <!-- founder section  -->
-<section class=" COUNT mb-12 my-10 relative bg-blue-900 flex justify-center items-center" style="height: 90vh;">
+<section class=" COUNT grid grid-cols-12 mb-12 my-10 relative bg-blue-900 select-none" style="height: 90vh;">
 
-    <div class=" w-80 h-80 rounded-full absolute right-20 z-30 flex justify-center items-center pt-5">
-        <div class=" BG bg-red-600 absolute top-0 rounded-t-full rounded-b-md" style="width: 330px; height:200px;"></div>
-        <img class="rounded-full z-20" src="images/unsplash.png" alt="" style="width: 300px; height:300px;">
-        <div class=" w-28 h-28 bg-red-700 rounded-full absolute -left-4 -bottom-2 z-30 flex justify-center items-center">
-            <div class="w-24 h-24 bg-white rounded-full"></div>
+    <div class=" h-full w-full flex absolute">
+        <div class=" w-1/2 h-full flex items-center justify-center py-32 pl-28">
+            <div class="text-6xl font-semibold select-none">
+                <h1 class=" mb-2 text-white">Opportunities</h1>
+                <h1 class=" mb-2 text-white"> don't happen, You </h1>
+                <h1 class=" mb-2 text-blue-500 text-7xl font-bold">create them.</h1>
+            </div>
+        </div>
+        <div class=" w-1/2 h-full flex items-center justify-start">
+
+            <div class=" w-80 h-80 rounded-full absolute right-20 z-30 flex justify-center items-center pt-5">
+                <div class=" BG absolute top-6 -right-16 rounded-t-full rounded-b-md rotate-45" style="width: 330px; height:175px;"></div>
+                <img class="rounded-full z-20" src="images/unsplash.png" alt="" style="width: 300px; height:300px;">
+                <div class=" w-28 h-28 bg-blue-500 rounded-full absolute left-5 -bottom-10 z-30 flex justify-center items-center">
+                    <img src="AP/4.svg" alt="" class=" w-10 h-10 absolute -top-4 -left-8">
+                    <div class="w-24 h-24 bg-white rounded-full grid grid-cols-2 gap-2 opacity-45">
+                        <a class=" text-black col-span-2 flex items-end justify-center" aria-label="Twitter link" href="#">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="icon w-7 h-7" viewBox="0 0 448 512">
+                                <path d="M64 32C28.7 32 0 60.7 0 96V416c0 35.3 28.7 64 64 64H384c35.3 0 64-28.7 64-64V96c0-35.3-28.7-64-64-64H64zm297.1 84L257.3 234.6 379.4 396H283.8L209 298.1 123.3 396H75.8l111-126.9L69.7 116h98l67.7 89.5L313.6 116h47.5zM323.3 367.6L153.4 142.9H125.1L296.9 367.6h26.3z"/>
+                            </svg>
+                        </a>
+                        <a class=" text-black col-span-1 flex justify-end" aria-label="Facebook link" href="#">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="icon w-7 h-7" width="1rem" height="1rem"
+                                viewBox="0 0 512 512">
+                                <path fill="currentColor"
+                                    d="M455.27,32H56.73A24.74,24.74,0,0,0,32,56.73V455.27A24.74,24.74,0,0,0,56.73,480H256V304H202.45V240H256V189c0-57.86,40.13-89.36,91.82-89.36,24.73,0,51.33,1.86,57.51,2.68v60.43H364.15c-28.12,0-33.48,13.3-33.48,32.9V240h67l-8.75,64H330.67V480h124.6A24.74,24.74,0,0,0,480,455.27V56.73A24.74,24.74,0,0,0,455.27,32Z">
+                                </path>
+                            </svg>
+                        </a>
+                        <a class=" text-black col-span-1" aria-label="Linkedin link" href="#">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="icon w-7 h-7" width="1rem" height="1rem"
+                                viewBox="0 0 512 512">
+                                <path fill="currentColor"
+                                    d="M444.17,32H70.28C49.85,32,32,46.7,32,66.89V441.61C32,461.91,49.85,480,70.28,480H444.06C464.6,480,480,461.79,480,441.61V66.89C480.12,46.7,464.6,32,444.17,32ZM170.87,405.43H106.69V205.88h64.18ZM141,175.54h-.46c-20.54,0-33.84-15.29-33.84-34.43,0-19.49,13.65-34.42,34.65-34.42s33.85,14.82,34.31,34.42C175.65,160.25,162.35,175.54,141,175.54ZM405.43,405.43H341.25V296.32c0-26.14-9.34-44-32.56-44-17.74,0-28.24,12-32.91,23.69-1.75,4.2-2.22,9.92-2.22,15.76V405.43H209.38V205.88h64.18v27.77c9.34-13.3,23.93-32.44,57.88-32.44,42.13,0,74,27.77,74,87.64Z">
+                                </path>
+                            </svg>
+                        </a>
+                    </div>
+                </div>
+            </div>
+
         </div>
     </div>
 
-    <div class=" w-32 h-32 bg-blue-600 absolute top-0 left-0 rounded-br-full"></div>
-    <div class=" w-80 h-80 bg-blue-600 absolute bottom-0 right-0 rounded-tl-full"></div>
+    {{-- <div class=" w-80 h-80 rounded-full absolute right-20 z-30 flex justify-center items-center pt-5">
+        <div class=" BG absolute top-0 rounded-t-full rounded-b-md" style="width: 330px; height:175px;"></div>
+        <img class="rounded-full z-20" src="images/unsplash.png" alt="" style="width: 300px; height:300px;">
+        <div class=" w-28 h-28 bg-red-700 rounded-full absolute -left-4 -bottom-2 z-30 flex justify-center items-center">
+            <img src="AP/4.svg" alt="" class=" w-10 h-10 absolute -top-4 -left-8">
+            <div class="w-24 h-24 bg-white rounded-full"></div>
+        </div>
+    </div> --}}
+
+    {{-- top bottom  --}}
+    <div class="">
+        <div class=" w-28 h-28 bg-blue-600 absolute top-0 left-0 rounded-br-full"></div>
+        <div class=" w-72 h-72 bg-blue-600 absolute bottom-0 right-0 rounded-tl-full"></div>
+    </div>
     
+    {{-- icons  --}}
+    <div class="">
+        <img src="AP/2.svg" alt="" class=" w-14 h-14 absolute top-10 right-10">
+        <img src="AP/3.svg" alt="" class=" absolute left-28 top-24 w-20 h-20">
+        
+        <img src="AP/1.svg" alt="" class=" w-20 h-20 absolute bottom-10 left-20">
+        <img src="AP/3.svg" alt="" class=" w-24 h-24 absolute bottom-6 right-20 ">
+    </div>
+
+    {{-- <div class=" grid grid-cols-12 bg-black h-56 absolute top-20 left-20">
+        <div class=" grid grid-cols-5 col-span-1">
+            <div class="col-span-1 h-14 absolute top-20 left-20 bg-red-500"></div>
+            <div class="col-span-1 h-14 absolute top-20 left-20 bg-red-500"></div>
+            <div class="col-span-1 h-14 absolute top-20 left-20 bg-red-500"></div>
+            <div class="col-span-1 h-14 absolute top-20 left-20 bg-red-500"></div>
+            <div class="col-span-1 h-14 absolute top-20 left-20 bg-red-500"></div>
+            <div class="col-span-1 h-14 absolute top-20 left-20 bg-red-500"></div>
+            <div class="col-span-1 h-14 absolute top-20 left-20 bg-red-500"></div>
+            <div class="col-span-1 h-14 absolute top-20 left-20 bg-red-500"></div>
+            <div class="col-span-1 h-14 absolute top-20 left-20 bg-red-500"></div>
+            <div class="col-span-1 h-14 absolute top-20 left-20 bg-red-500"></div>
+        </div>
+    </div> --}}
+
     {{-- <div class="grid grid-rows-4 grid-flow-col gap-4 bg-red-600">
         <div class="row-start-2 row-span-2 w-32 h-32 bg-blue-600">01</div>
         <div class="row-end-3 row-span-2   w-32 h-32 bg-blue-600">02</div>
