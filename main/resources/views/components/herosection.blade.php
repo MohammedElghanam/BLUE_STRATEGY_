e4f3cc5569e8c3ae6aaff0c705302f8f34bced4f
Mohammed Elghanam <144786058+MohammedElghanam@users.noreply.github.com>
2024-07-05 16:22:54 +0100

    update

--- main/resources/views/components/herosection.blade.php
+++ main/resources/views/components/herosection.blade.php
@@ -57,7 +57,7 @@
 <section class="mx-8 section-bg" style="min-height: 75vh;">
     <div id="FLOAT"></div>
     <div id="FLOAT1"></div>
-    <div class="w-full h-10 bg-blue-600"></div>
+    <div class="w-full h-20"></div>
     <div class="grid max-w-screen-xl py-8 lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12 h-full mx-16">
         <div class="mr-auto place-self-center lg:col-span-7 mb-12 lg:mb-0">
             <h1 class="max-w-2xl mb-5 text-4xl font-extrabold leading-none md:text-5xl xl:text-6xl dark:text-white">
@@ -67,45 +67,33 @@
                 From checkout to global sales tax compliance, companies around the world use Flowbite to simplify their payment stack.
             </p>
             <div class="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-6 gap-6 w-full mb-10">
-                <div class="p-1 rounded-md hover:scale-105 transition-transform">
-                    <div class="flex justify-center items-center p-2 rounded-full col-span-1">
-                        <img class="w-9 h-9" src="AP/100.png" alt="">
-                    </div>
-                    <h1 class="text-center font-medium text-blue-900">TPE/PME</h1>
+                <div class="p-1 rounded-md hover:scale-105 transition-transform  col-span-1 flex flex-col justify-center items-center gap-2">
+                        <img class="w-9 h-9" src="AP/112.png" alt="">
+                    <h1 class="text-center font-medium text-blue-900 ">TPE/PME</h1>
                 </div>
-                <div class="p-1 rounded-md hover:scale-105 transition-transform">
-                    <div class="flex justify-center items-center p-2 rounded-full col-span-1">
-                        <img class="w-9 h-9" src="AP/101.png" alt="">
-                    </div>
+                <div class="p-1 rounded-md hover:scale-105 transition-transform  col-span-1 flex flex-col justify-center items-center gap-2">
+                        <img class="w-9 h-9" src="AP/110.png" alt="">
                     <h1 class="text-center font-medium text-blue-900">Cooperative</h1>
                 </div>
-                <div class="p-1 rounded-md hover:scale-105 transition-transform">
-                    <div class="flex justify-center items-center p-2 rounded-full col-span-1">
-                        <img class="w-9 h-9" src="AP/102.png" alt="">
-                    </div>
-                    <h1 class="text-center font-medium text-blue-900">Etablissement</h1>
+                <div class="p-1 rounded-md hover:scale-105 transition-transform  col-span-1 flex flex-col justify-center items-center gap-2">
+                        <img class="w-9 h-9" src="AP/111.png" alt="">
+                        <h1 class="text-center font-medium text-blue-900 ">Etablissement</h1>
                 </div>
-                <div class="p-1 rounded-md hover:scale-105 transition-transform">
-                    <div class="flex justify-center items-center p-2 rounded-full col-span-1">
-                        <img class="w-9 h-9" src="AP/103.png" alt="">
-                    </div>
+                <div class="p-1 rounded-md hover:scale-105 transition-transform  col-span-1 flex flex-col justify-center items-center gap-2">
+                        <img class="w-9 h-9" src="AP/109.png" alt="">
                     <h1 class="text-center font-medium text-blue-900">Association</h1>
                 </div>
-                <div class="p-1 rounded-md hover:scale-105 transition-transform">
-                    <div class="flex justify-center items-center p-2 rounded-full col-span-1">
-                        <img class="w-9 h-9" src="AP/104.png" alt="">
-                    </div>
-                    <h1 class="text-center font-medium text-blue-900">Startup</h1>
+                <div class="p-1 rounded-md hover:scale-105 transition-transform  col-span-1 flex flex-col justify-center items-center gap-2">
+                        <img class="w-9 h-9" src="AP/108.png" alt="">
+                    <h1 class="text-center font-medium text-blue-900 ">Startup</h1>
                 </div>
-                <div class="p-1 rounded-md hover:scale-105 transition-transform">
-                    <div class="flex justify-center items-center p-2 rounded-full col-span-1">
-                        <img class="w-9 h-9" src="AP/105.png" alt="">
-                    </div>
-                    <h1 class="text-center font-medium text-blue-900">Professional</h1>
+                <div class="p-1 rounded-md hover:scale-105 transition-transform  col-span-1 flex flex-col justify-center items-center gap-2">
+                        <img class="w-9 h-9 " src="AP/107.png" alt="">
+                    <h1 class="text-center font-medium text-blue-900 ">Professional</h1>
                 </div>
             </div>
             <a href="{{route('booking_now')}}"
-               class="inline-flex items-center justify-center px-5 py-3 mr-3 mb-5 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
+               class="inline-flex items-center justify-center px-5 py-3 mr-3 mb-5 text-base font-medium text-center text-white rounded-lg bg-blue-900 hover:bg-blue-800">
                 Booking Now
                 <svg class="w-5 h-5 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20"
                      xmlns="http://www.w3.org/2000/svg">
@@ -113,10 +101,6 @@
                           d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
                           clip-rule="evenodd"></path>
                 </svg>
-            </a>
-            <a href="#contact"
-               class="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-gray-900 border border-blue-700 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800">
-                Contact
             </a>
         </div>
         <!-- Conditionally remove this div on md screens -->
