f06eae0c214209bcbdc18a4bcfb44e1570d4d7af
Mohammed Elghanam <144786058+MohammedElghanam@users.noreply.github.com>
2024-07-10 16:12:45 +0100

    update

--- main/resources/views/components/Vision.blade.php
+++ main/resources/views/components/Vision.blade.php
@@ -1,1 +1,60 @@
-hello
+<style>
+    .SCAL:hover{
+        transform: scale(1.1);
+        transition: 1s;
+    }
+</style>
+
+<div class=" bg-white pt-28">
+    <div class=" grid grid-cols-12 px-20 w-full bg-white">
+        <div class=" col-span-12 h-96 relative">
+            <img src="AP/91.png" alt="fo bar" class=" h-96 w-full">
+            <img src="AP/22.png" alt="" class=" absolute right-40 -bottom-40 w-68 h-64">
+            <div class=" flex items-end justify-start w-1/2 absolute bottom-0 left-0">
+                <div class=" w-96 h-28 bg-white flex items-end justify-center">
+                    <div class=" w-96 h-1 bg-blue-900"></div>
+                </div>
+                <div class=" h-16 w-40 bg-white"></div>
+            </div>
+        </div>
+        <div class=" w-32 bg-blue-900 opacity-70 h-3/4 absolute top-[84px] right-1/3"></div>
+        <div class="  col-span-7 mt-4">
+            <h1 class=" text-7xl font-semibold text-gray-900 mb-5">Nos secteur d'activite</h1>
+            <p class=" text-lg font-normal text-gray-900 mb-4">
+                BLUE STRATEGY développe des compétences sectorielles pour mettre en place des solutions qui anticipent ou
+                répondent aux attentes de différents clients .
+            </p>
+            <p class=" text-lg font-normal text-gray-900">
+                otre cabinet intervient dans divers secteurs d'activité, aussi bien au sein d'entreprises que d'organisations de
+                toutes tailles, que ce soit dans le secteur public ou privé.
+            </p>
+        </div>
+    </div>
+</div>
+
+<div class=" grid grid-cols-12 gap-8 p-20 w-full bg-red-600 ">
+    <div class=" SCAL col-span-3 "><img class=" rounded-xl" src="VI/125.png" alt=""> <h1 class=" pl-2 text-lg font-medium">Sport</h1> </div>
+    <div class=" SCAL col-span-3 "><img class=" rounded-xl" src="VI/126.png" alt=""> <h1 class=" pl-2 text-lg font-medium">Education</h1> </div>
+    <div class=" SCAL col-span-3 "><img class=" rounded-xl" src="VI/127.png" alt=""> <h1 class=" pl-2 text-lg font-medium">Culture et art</h1> </div>
+    <div class=" SCAL col-span-3 "><img class=" rounded-xl" src="VI/124.png" alt=""> <h1 class=" pl-2 text-lg font-medium">Artisanat</h1> </div>
+    
+    <div class=" SCAL col-span-3 "><img class=" rounded-xl" src="VI/128.png" alt=""> <h1 class=" pl-2 text-lg font-medium">Developpement durabl</h1> </div>
+    <div class=" SCAL col-span-3 "><img class=" rounded-xl" src="VI/129.png" alt=""> <h1 class=" pl-2 text-lg font-medium">Environnement et energie</h1> </div>
+    <div class=" SCAL col-span-3 "><img class=" rounded-xl" src="VI/124.png" alt=""> <h1 class=" pl-2 text-lg font-medium">Artisanat</h1> </div>
+    <div class=" SCAL col-span-3 "><img class=" rounded-xl" src="VI/124.png" alt=""> <h1 class=" pl-2 text-lg font-medium">Artisanat</h1> </div>
+    
+    <div class=" SCAL col-span-3 "><img class=" rounded-xl" src="VI/125.png" alt=""> <h1 class=" pl-2 text-lg font-medium">Sport</h1> </div>
+    <div class=" SCAL col-span-3 "><img class=" rounded-xl" src="VI/126.png" alt=""> <h1 class=" pl-2 text-lg font-medium">Education</h1> </div>
+    <div class=" SCAL col-span-3 "><img class=" rounded-xl" src="VI/124.png" alt=""> <h1 class=" pl-2 text-lg font-medium">Artisanat</h1> </div>
+    <div class=" SCAL col-span-3 "><img class=" rounded-xl" src="VI/124.png" alt=""> <h1 class=" pl-2 text-lg font-medium">Artisanat</h1> </div>
+    
+    <div class=" SCAL col-span-3 "><img class=" rounded-xl" src="VI/125.png" alt=""> <h1 class=" pl-2 text-lg font-medium">Sport</h1> </div>
+    <div class=" SCAL col-span-3 "><img class=" rounded-xl" src="VI/126.png" alt=""> <h1 class=" pl-2 text-lg font-medium">Education</h1> </div>
+    <div class=" SCAL col-span-3 "><img class=" rounded-xl" src="VI/124.png" alt=""> <h1 class=" pl-2 text-lg font-medium">Artisanat</h1> </div>
+    <div class=" SCAL col-span-3 "><img class=" rounded-xl" src="VI/124.png" alt=""> <h1 class=" pl-2 text-lg font-medium">Artisanat</h1> </div>
+
+    <div class=" SCAL col-span-3 "><img class=" rounded-xl" src="VI/125.png" alt=""> <h1 class=" pl-2 text-lg font-medium">Sport</h1> </div>
+    <div class=" SCAL col-span-3 "><img class=" rounded-xl" src="VI/126.png" alt=""> <h1 class=" pl-2 text-lg font-medium">Education</h1> </div>
+    <div class=" SCAL col-span-3 "><img class=" rounded-xl" src="VI/124.png" alt=""> <h1 class=" pl-2 text-lg font-medium">Artisanat</h1> </div>
+    <div class=" SCAL col-span-3 "><img class=" rounded-xl" src="VI/124.png" alt=""> <h1 class=" pl-2 text-lg font-medium">Artisanat</h1> </div>
+</div>
