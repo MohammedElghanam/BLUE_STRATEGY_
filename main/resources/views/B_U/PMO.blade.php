85a7c0e07cfb50c1fe49738f8bcf5fd9c8587417
Mohammed Elghanam <144786058+MohammedElghanam@users.noreply.github.com>
2024-07-15 11:59:39 +0100

    Update PMO.blade.php

--- main/resources/views/B_U/PMO.blade.php
+++ main/resources/views/B_U/PMO.blade.php
@@ -15,19 +15,18 @@
 }
 </style>
 
-<section class=" pt-20">
-    <div class=" flex justify-center items-center relative">
-        <img class=" " src="B_U/8.png" alt="">
-        <div class="CO w-2/5 h-full absolute bottom-0 left-28 opacity-70"></div>
-        <div class="BOR w-3/4 h-3/5 absolute">
-            <div class=" COLOR w-52 h-52 absolute -bottom-20 -left-1.5">
-                <div class=" text-white absolute -right-40 bottom-24">
-                    <h1 class=" font-bold text-4xl">B.U 01</h1>
-                    <h1 class=" font-bold text-4xl">PMO</h1>
-                    <p class=" font-normal -right-44">PROJECT MANAGMENT OFFICE</p>
-                </div>
+<section class="pt-20">
+    <div class="flex justify-center items-center relative">
+        <img class="w-full md:w-auto" src="B_U/8.png" alt="">
+        <div class="CO w-full md:w-2/5 h-full absolute bottom-0 left-0 md:left-28 opacity-70"></div>
+        <div class="BOR w-full md:w-3/4 h-3/5 absolute p-4 md:p-0">
+            <div class="COLOR w-40 h-40 sm:w-52 sm:h-52 absolute -bottom-10 md:-bottom-20 left-1 md:-left-1.5"></div>
+            <div class="text-white absolute left-1/2 transform -translate-x-1/2 md:-right-40 md:bottom-4 text-center md:text-left">
+                <h1 class="font-bold text-2xl md:text-4xl">B.U 01</h1>
+                <h1 class="font-bold text-2xl md:text-4xl">PMO</h1>
+                <p class="font-normal">PROJECT MANAGEMENT OFFICE</p>
             </div>
-            <div class="polygon absolute right-4 bottom-4 bg-blue-500 w-32 h-28"></div>
+            <div class="polygon absolute right-1 bottom-1 md:right-4 md:bottom-4 bg-blue-500 w-16 h-14 md:w-32 md:h-28"></div>
         </div>
     </div>
 </section>
